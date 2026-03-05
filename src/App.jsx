3fe20a3faabb534df0f6f2657a79e71c19a728bf
Rcrowley-UVAgit <pps7jp@virginia.edu>
2026-03-04 20:18:07 -0500

    Fix routing: add basename to BrowserRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function AppRoutes() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Drops/">
       <AuthProvider>
         <GroupsProvider>
           <AppRoutes />
